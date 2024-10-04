c5c8258a1135e82bffd98aaa8cf3a011503d4eaf
akinwunmi-akinrimisi <akinolaakinrimisi@gmail.com>
2024-10-04 07:48:39 +0100

    deployed

--- ignition/modules/SaveERC20.ts
+++ ignition/modules/SaveERC20.ts
@@ -13,4 +13,4 @@ export default SaveERC20Module;
 
 
 // Web3CXIModule#Web3CXI - 0x35848e5d5bB18eb5470Cacc81915180b97FD872c
-// SaveERC20Module#SaveERC20 - 0x21D2Bb19Cf3f5445aAE5A41f1A744dC6A6333BdC
+// SaveERC20Module#SaveERC20 - 
